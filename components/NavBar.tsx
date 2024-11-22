b87e4e1e65db07beb51247d347c598a85ef3a6b7
Hareem2134 <007hareem@gmail.com>
2024-11-23 01:40:18 +0500

    update

---
+++ components/NavBar.tsx
@@ -0,0 +1,10 @@
+export default function NavBar(){
+    return(
+        <div className="nav-bar">
+
+            <div className="grid place-items-center py-3 text-white text-2xl font-bold bg-gradient-to-br from-gray-900 via-blue-800 to-gray-900">
+                TO-DO LIST APPLICATION
+            </div>
+            
+        </div>);
+}
